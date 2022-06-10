36c907dc9d60692243d474a49fa4ab6846651ee5
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-06-10 16:57:08 +0100

    raise an error if the nationality has not been found

--- lib/famous_people.rb
+++ lib/famous_people.rb
@@ -4,10 +4,18 @@ class FamousPeople
   end
 
   def find_by_nationality(nationality)
-    @people.select do |person, persons_nationality|
+    matched_person = @people.select do |person, persons_nationality|
       if persons_nationality == nationality
         @people[person]
       end
     end.keys
+
+    error_message = 'Nationality not matched!'
+
+    if matched_person == []
+      raise error_message
+    else
+      matched_person
+    end
   end
 end
