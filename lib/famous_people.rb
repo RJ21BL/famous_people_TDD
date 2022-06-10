1fc8209c0740797c8a097fc58bb19b3e44820c7c
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-06-10 13:47:47 +0100

    implement code find the matching person for given nationality

--- lib/famous_people.rb
+++ lib/famous_people.rb
@@ -3,6 +3,11 @@ class FamousPeople
     @people = people
   end
 
-  def find_by_nationality
+  def find_by_nationality(nationality)
+    @people.select do |person, persons_nationality|
+      if persons_nationality == nationality
+        @people[person]
+      end
+    end.keys
   end
 end
